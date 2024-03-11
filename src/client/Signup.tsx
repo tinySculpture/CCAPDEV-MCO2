106fbf88f3f3e9c985493452f6ae3d7795d8a22e
tinySculpture <jmluistro1@gmail.com>
2024-03-11 19:53:59 +0800

    added mongo and mongoose

--- src/client/Signup.tsx
+++ src/client/Signup.tsx
@@ -2,19 +2,21 @@ import { Link } from "react-router-dom"
 import Navbar from "./components/Navbar"
 
 const Signup = () => {
-
+  
   return(
     <div>
       <Navbar isLoggedIn={false} />
       
       <div className="container d-flex d-lg-flex justify-content-lg-center align-items-lg-center" style={{height: "100%", display: "flex"}}>
-        <div className="card mb-5 bg-primary" style={{marginBottom: "0px", width: "30%"}}>
+        <div className="card mb-5" style={{marginBottom: "0px", width: "30%"}}>
             <div className="card-body d-flex flex-column align-items-center">
                 <form className="text-center" method="post" style={{width: "90%"}}>
                     <h1 className="text-start" style={{marginBottom: "40px"}}><strong>Sign up</strong></h1>
+                    <div className="mb-3"><input className="form-control" type="text" name="username" placeholder="Username" /></div>
+                    <div className="mb-3"><input className="form-control" type="text" name="fullname" placeholder="Full Name" /></div>
                     <div className="mb-3"><input className="form-control" type="email" name="email" placeholder="Email" /></div>
                     <div className="mb-3"><input className="form-control" type="password" name="password" placeholder="Password" /></div>
-                    <div className="mb-3"><button className="btn btn-primary d-block w-100" type="submit">Login</button></div>
+                    <div className="mb-3"><button className="btn btn-primary d-block w-100" type="submit">Register</button></div>
                     <p className="text-muted">Already have an account? <Link to="/login">Login.</Link></p>
                 </form>
             </div>
--- src/server/main.js
+++ src/server/main.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import ViteExpress from "vite-express";
 
 const PORT = 3000
@@ -6,5 +7,12 @@ const PORT = 3000
 const app = express();
 
 ViteExpress.listen(app, PORT, () =>
-  console.log("Server is listening on port 3000..."),
+console.log("Server is listening on port 3000..."),
 );
+
+const mongoose = mongoose();
+
+mongoose.connect("mongodb+srv://josiah:josiahmari1@edurevs.4cnhyvi.mongodb.net/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
